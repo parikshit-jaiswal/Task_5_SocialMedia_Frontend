9a4cac99e608d3413cb55cef77d604e9093aa76d
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2024-11-08 17:05:44 +0530

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div className="m-5">
+        <Button variant='destructive'>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
